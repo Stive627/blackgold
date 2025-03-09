395471984b7336d9bb979ff6c570b4536c803d7f
TSA <tsasoft7@gmail.com>
2025-03-09 21:02:09 +0530

    last

--- app/page.js
+++ app/page.js
@@ -3,11 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <p className=' text-center text-3xl text-blue-500'>Welcome to our next page!</p>
-      <p >{process.env.name}</p>
-      <Link href={'/about'}>Go to about page</Link><br/>
-      <Link href={'/posts/1'}>Go to the post 1 </Link><br/>
-      <Link href={'/posts/2'}>Go to the post 2 </Link>
+      <p className=' text-center text-3xl text-blue-500'>Welcome to Blackgold!</p>
     </div>
   );
 }
