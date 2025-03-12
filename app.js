a819274bdb9e3061beaf115dafbc7a61c24d13ea
TSA <tsasoft7@gmail.com>
2025-03-12 17:34:09 +0530

    deployment solved

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 require('dotenv').config()
 const port = process.env.port || 8080
-const app = express()
+const app = express() 
 app.use(express.static('public'))
 app.get('/', (req, res)=>res.send('The api is working normally.'))
 app.listen(port, ()=>console.log(`The server is running at http://localhost:${port}`))
