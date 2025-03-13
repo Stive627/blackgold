e501982a39f6ae76d591ec45ae42eca012af42a1
TSA <tsasoft7@gmail.com>
2025-03-13 18:04:14 +0530

    last commit

--- Dockerfile
+++ Dockerfile
@@ -14,7 +14,7 @@ RUN npm install
 COPY . .
 
 # Expose the port your app uses
-EXPOSE 8080
+EXPOSE 3000
 
 # Run the application
 CMD ["node", "app.js"]
--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 require('dotenv').config()
-const port = process.env.port || 8080
+const port = process.env.port
 const app = express() 
 app.use(express.static('public'))
 app.get('/', (req, res)=>res.send('The api is working normally.'))
