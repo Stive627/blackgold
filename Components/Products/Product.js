eb49bd31da50d793a11ed62aa3a8f9020d74a7d6
Stive627 <fossistive627@gmail.com>
2025-04-10 22:12:00 +0530

    third commit

--- Components/Products/Product.js
+++ Components/Products/Product.js
@@ -6,8 +6,10 @@ function Product({products, setProducts}) {
   const large = useScreen()
   if(!products) return<div className=' mt-3  w-full h-64 flex justify-center items-center'><div className=' w-10 h-10 animate-spin rounded-full border border-blue-900 border-t-white'></div></div>
   return (
-    <div className={`grid gap-3 mt-3 ${large?'grid-cols-5':'grid-cols-2'}`}>
-      {products.map((elt, indx)=><ProductCard key={indx} product={elt}/>)}
+    <div className=' w-screen flex justify-center'>
+      <div style={{width:'98%'}} className={`grid gap-3 mt-3 ${large?'grid-cols-5':'grid-cols-2'}`}>
+        {products.map((elt, indx)=><ProductCard key={indx} product={elt}/>)}
+      </div>
     </div>
   )
 }
