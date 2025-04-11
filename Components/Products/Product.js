fc52627b41ac351d623cc1950a290f27c9ac238d
Stive627 <fossistive627@gmail.com>
2025-04-11 23:09:22 +0530

    last commit

--- Components/Products/Product.js
+++ Components/Products/Product.js
@@ -7,7 +7,7 @@ function Product({products, setProducts}) {
   if(!products) return<div className=' mt-3  w-full h-64 flex justify-center items-center'><div className=' w-10 h-10 animate-spin rounded-full border border-blue-900 border-t-white'></div></div>
   return (
     <div className=' w-screen flex justify-center'>
-      <div style={{width:'98%'}} className={`grid gap-3 mt-3 ${width <1600 && width >1400 && 'grid-cols-4'} ${width <1400 && width >1060 && 'grid-cols-3'} ${width <1060 && 'grid-cols-2'} ${width>=1600 && 'grid-cols-5'}`}>
+      <div style={{width:'98%'}} className={`grid gap-3 mt-3 ${width <1700 && width >1200 && 'grid-cols-4'} ${width <1200 && width >860 && 'grid-cols-3'} ${width <860 && 'grid-cols-2'} ${width>=1700 && 'grid-cols-5'}`}>
         {products.map((elt, indx)=><ProductCard key={indx} product={elt}/>)}
       </div>
     </div>
