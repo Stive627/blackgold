6a1751b454ba3078b8b50b90ac41d73154b331c8
Stive627 <fossistive627@gmail.com>
2025-04-12 16:19:49 +0530

    8th commit

--- Components/Category/Category.js
+++ Components/Category/Category.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import React from 'react'
 import { useScreen } from '../../hooks/useScreen'
 
-const CategoryCard = ({title, subTitle, handleClick}) => {
+const CategoryCard = ({title, subTitle, handleClick, img}) => {
     return(
         <div className=' relative   '>
             <div className='text-white absolute  left-7 top-3'>
@@ -10,20 +10,19 @@ const CategoryCard = ({title, subTitle, handleClick}) => {
                 <p  className=' text-[16px] '>{subTitle}</p>
             </div>
             <div className=' absolute bottom-3.5 left-7'> <button onClick={()=> handleClick()} style={{width:158, height:35, backgroundColor:'white'}} className=' text-[16px] border border-white rounded-lg cursor-pointer'>Shop Now</button></div>
-            <Image alt='farm fresh fruits' src={'https://blackgoldapi.tsasoft.com/Fruits.jpg'} className=' rounded-lg' style={{width:'612px', height:'288px'}} width={612} height={288}/>
+            <Image alt='farm fresh fruits' src={`https://blackgoldapi.tsasoft.com/${img}`} className=' rounded-lg'  width={900} height={900}/>
         </div>
     )
 }
 function Category() {
     const width = useScreen()
-if(width > 1000)
   return (
     <div className=' flex justify-center'>
         <div style={{width:'98%'}} className=' w-full flex justify-center mt-2 '>
             <div className=' flex justify-between gap-5'>
-                <CategoryCard title={'Farm-Fresh Fruits'} subTitle={'Ripe, delicious, and straight from the source!'} handleClick={()=>console.log()}/>
-                <CategoryCard title={'Organic Vegetables'} subTitle={'Nutritious, flavorful, and always fresh!'} handleClick={()=>console.log()}/>
-                <CategoryCard title={'Organic Vegetables'} subTitle={'Nutritious, flavorful, and always fresh!'} handleClick={()=>console.log()}/>
+                <CategoryCard title={'Farm-Fresh Fruits'} subTitle={'Ripe, delicious, and straight from the source!'} handleClick={()=>console.log()} img={'Fruits.jpg'}/>
+                {width > 624 && <CategoryCard title={'Organic Vegetables'} subTitle={'Nutritious, flavorful, and always fresh!'} handleClick={()=>console.log()} img={'Regional.png'}/>}
+                {width > 1000 && <CategoryCard title={'Organic Vegetables'} subTitle={'Nutritious, flavorful, and always fresh!'} handleClick={()=>console.log()} img={'Regional.png'}/>}
             </div>
         </div>
     </div>
