a4f17af40534e9bd2b5529fee895dcaddaac04db
Stive627 <fossistive627@gmail.com>
2025-04-27 10:48:44 +0530

    i think i have finished with the logo animation

--- Components/Logo.js
+++ Components/Logo.js
@@ -5,10 +5,7 @@ import '../App.css'
 
 function Logo() {
   return (
-    <div className=' relative bg-gray-200'>
-        <div className=' absolute w-86 h-86 logoblack  top-7 left-7 rounded-full'> </div>
-      <Image alt='blackgold logo' width={400} height={400} src={fetchLink('logo.png')}/>
-    </div>
+    <Image alt='blackgold logo' className='logoblack rounded-full' width={400} height={400} src={fetchLink('logoAnim.png')}/>
   )
 }
 
