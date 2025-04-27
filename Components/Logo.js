242abb06f7df223ac0ec9db80d18f85192c018eb
Stive627 <fossistive627@gmail.com>
2025-04-27 12:44:23 +0530

    size adjusted

--- Components/Logo.js
+++ Components/Logo.js
@@ -1,11 +1,15 @@
+"use client"
 import Image from 'next/image'
 import React from 'react'
 import { fetchLink } from '../Functions/fetchLink'
 import '../App.css'
+import { useScreen } from '../hooks/useScreen'
 
 function Logo() {
+  const width = useScreen()
+  const large = width > 600
   return (
-    <Image alt='blackgold logo' className='logoblack rounded-full' width={300} height={300} src={fetchLink('logoAnim.png')}/>
+    <Image alt='blackgold logo' className='logoblack rounded-full' width={large ? 200 : 150} height={ large ? 200 : 150} src={fetchLink('logoAnim.png')}/>
   )
 }
 
