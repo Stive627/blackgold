da7a3c11eabb22f242504d5632c71ded2a005436
Stive627 <fossistive627@gmail.com>
2025-04-30 18:43:17 +0530

    i have configured the location

--- Components/Location/Location.js
+++ Components/Location/Location.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { useScreen } from '../../hooks/useScreen';
 
-function Location({showl, setShowl, children=''}) {
+function Location({showl, setShowl, children='', handleEnableLocation}) {
     const width = useScreen()
     const [search, setSearch] = useState('')
   return (
@@ -12,7 +12,7 @@ function Location({showl, setShowl, children=''}) {
                         <div onClick={(e)=> e.stopPropagation()} style={{width:width>725?'700px':width>500?'500px':width > 400?'380px':'340px'}} className={`rounded-lg bg-white h-80 py-2 px-3 ${width<1000? 'mt-26':'mt-4'}`}>
                             <p className=' text-[17px] font-semibold text-black text-center'>Location</p>
                             <hr style={{color:'rgba(207, 207, 207, 1)'}} className='w-full mb-5'/>
-                            <div className=' flex justify-center text-white mb-3'><button style={{backgroundColor:'rgba(0, 122, 94, 1)'}} className=' px-2 py-1 rounded-lg cursor-pointer'>Enable Location access</button></div>
+                            <div className=' flex justify-center text-white mb-3'><button onClick={handleEnableLocation}  style={{backgroundColor:'rgba(0, 122, 94, 1)'}} className=' px-2 py-1 rounded-lg cursor-pointer'>Enable Location access</button></div>
                             <p className=' text-center text-black'>OR</p>
                             <div className=' flex flex-row justify-center'>
                                 <input style={{borderColor:'rgba(217, 217, 217, 0.82)', backgroundColor:'rgba(200, 200, 200,0.2)'}} placeholder='Search location' className='border grow w-full py-1 rounded-lg px-1 text-black placeholder-black outline-none' value={search} onChange={(e)=>setSearch(e.target.value)}/>
--- Components/Navbar/Navbar.js
+++ Components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import SearchIcon from '@mui/icons-material/Search';
@@ -9,11 +9,47 @@ import { useScreen } from '../../hooks/useScreen';
 import Language from '../Language/Language';
 import Location from '../Location/Location';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import axios from 'axios';
 
 
 function Navbar() {
     const width= useScreen()
     const [showl, setShowl] = useState(false)
+    const [streetName, setStreetName] = useState('')
+    const [locadata, setLocaData] = useState(undefined)
+    const alreadySetLocation = localStorage.getItem('blackgoldLocation')
+    function handleEnableLocation(){
+      const success = (pros) =>{
+        let coords = pros.coords
+        console.log(coords.latitude, coords.longitude)
+        setLocaData({longitude:coords.longitude, latitude:coords.latitude})    
+        localStorage.setItem('blackgoldLocation', JSON.stringify({longitude:coords.longitude, latitude:coords.latitude}))    
+      }
+      const fail = (err) => {
+        console.warn(err.code, err.message)
+      }
+      const options = {
+        enableHighAccuracy:true,
+        timeout:5000,
+        maximunAge:0
+      }
+      navigator.geolocation.getCurrentPosition(success, fail, options)
+    }
+    useEffect(()=>{
+      const data = JSON.parse(alreadySetLocation)
+      if(data){
+        console.log(data)
+        axios({url:`https://api.mapbox.com/search/geocode/v6/reverse?access_token=pk.eyJ1Ijoic3RpdmV0c2EiLCJhIjoiY21hM3Q4azhvMDBtdjJpcXhpaDRtYjB2OCJ9.cAVLQ69PTYp33gHCB6yV1A&longitude=${data.longitude}&latitude=${data.latitude}`, method:'GET'})
+        .then((value) => {
+          const street = value.data['features'][0]['properties'].full_address
+          console.log(street)
+          setStreetName(street)
+          setShowl(false)
+        })
+        .catch(err => console.log(err))
+      }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[locadata])
   
   if(width <1000){
     return(
@@ -29,10 +65,10 @@ function Navbar() {
         </div>
         <hr className=' w-full bg-white my-1.5'/>
         <div style={{color:'rgba(255, 255, 255, 1)'}} className=' w-full flex justify-between text-[14px] mt-2 py-2'>
-          <p><LocationOnIcon/> Delivering to Street 5th, Yaounde</p>
-          <button onClick={()=>setShowl(true)} className=' border border-none'>Change location</button>
+          {streetName && <p><LocationOnIcon/>{streetName}</p>}
+          <button onClick={()=>setShowl(true)} className=' border border-none'>{alreadySetLocation ? 'Change' : 'Add'} location</button>
         </div>
-        <Location showl={showl} setShowl={setShowl}></Location>
+        <Location handleEnableLocation={handleEnableLocation} showl={showl} setShowl={setShowl}></Location>
       </div>
     )
   }
@@ -40,10 +76,10 @@ function Navbar() {
     <div style={{backgroundColor:'rgba(41, 142, 119, 1)'}} className=' text-white w-full px-2 text-[14px] py-1 fixed z-20'>
         <div className=' flex flex-row items-center gap-5'>
           <Image width={50} height={50} alt='logo de blackgold' src={fetchLink('logo.png')}/>
-            <p className=' text-[24px] font-bold'>Black Gold</p>
-            <Location showl={showl} setShowl={setShowl}>
+            <p onClick={()=>{localStorage.clear('blackgoldLocation')}} className=' text-[24px] font-bold'>Black Gold</p>
+            <Location handleEnableLocation={handleEnableLocation} showl={showl} setShowl={setShowl}>
               <p>Deliver to </p>
-              <button onClick={()=>setShowl(true)} className=' border border-none '> Select Location {' '}<KeyboardArrowDownIcon className='text-white'/> </button>
+              {streetName ? <p>{streetName}</p> :<button onClick={()=>setShowl(true)} className=' border border-none '> Select Location {' '}<KeyboardArrowDownIcon className='text-white'/> </button>}
             </Location>
           <div className='relative grow'>
             <input className=' border border-none w-full py-2 bg-white text-slate-600 pl-2.5 rounded-md outline-none' placeholder='Search'/>
