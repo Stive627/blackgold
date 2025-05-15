6271e1d2b01f460d7abcbd771f69655721c37b86
Stive627 <fossistive627@gmail.com>
2025-05-15 22:01:42 +0530

    i have modified the logo source image

--- Components/Logo.js
+++ Components/Logo.js
@@ -6,7 +6,7 @@ import '../App.css'
 
 function Logo() {
   return (
-    <Image alt='blackgold logo' className='logoblack rounded-full' width={200} height={200} src={fetchLink('logoAnim.png')}/>
+    <Image alt='blackgold logo' className='logoblack rounded-full' width={200} height={200} src={'https://blackgold-bucket.s3.ap-south-1.amazonaws.com/logoAnim.png'}/>
   )
 }
 
